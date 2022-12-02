d4b0d7f9376891c5b1ab5a44b3caf83e5caf7dd1
JensToreFrem_iu3wszi <JensToreFrem_iu3wszi@LAPTOP-3S22G3IB>
2022-12-02 12:01:56 +0100

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] day01Input = File.ReadAllLines(@"c:\Temp\aoc22day01.txt");
+int caloriesGroup = 0;
+List<int> caloriesGroups = new List<int>();
+
+//part 1
+foreach (string day in day01Input)
+{
+    int calories;
+    bool notBlank = Int32.TryParse(day, out calories);
+
+    if (notBlank == true)
+    {
+        caloriesGroup += calories;
+    }
+    else
+    {
+        caloriesGroups.Add(caloriesGroup);
+        caloriesGroup = 0;
+    }
+}
+
+int indexOfMax = caloriesGroups.IndexOf(caloriesGroups.Max());
+Console.WriteLine($"Elf number {indexOfMax} carries most calories, {caloriesGroups[indexOfMax]}");
+
+// part 2
+caloriesGroups.Sort();
+caloriesGroups.Reverse();
+int topElves = caloriesGroups[0] + caloriesGroups[1] + caloriesGroups[2];
+Console.WriteLine($"The top 3 elves carry {topElves} calories");
---
+++ Day02/Program.cs
@@ -0,0 +1,49 @@
+﻿string[] day02Input = File.ReadAllLines(@"c:\Temp\aoc22day02.txt");
+int score = 0;
+int score2 = 0;
+
+foreach (string line in day02Input)
+{
+    switch(line)
+    {
+        case "A X":
+            score += 4;
+            score2 += 3;
+            break;
+        case "A Y":
+            score += 8;
+            score2 += 4;
+            break;
+        case "A Z":
+            score += 3;
+            score2 += 8;
+            break;
+        case "B X":
+            score += 1;
+            score2 += 1;
+            break;
+        case "B Y":
+            score += 5;
+            score2 += 5;
+            break;
+        case "B Z":
+            score += 9;
+            score2 += 9;
+            break;
+        case "C X":
+            score += 7;
+            score2 += 2;
+            break;
+        case "C Y":
+            score += 2;
+            score2 += 6;
+            break;
+        case "C Z":
+            score += 6;
+            score2 += 7;
+            break;
+    }
+}
+
+Console.WriteLine($"Total score on part 1 is {score}");
+Console.WriteLine($"Total score on part 2 is {score2}");
