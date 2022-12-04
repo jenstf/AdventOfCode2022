673fd443bed827392d9ae58666092dcabbc0a2d6
Jens Tore Fremmegård <jfremmegaard@lit.no>
2022-12-04 14:43:12 +0100

    Day04

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] day03Input = File.ReadAllLines(@"c:\temp\aoc22day04.txt");
+int firstFrom;
+int firstTo;
+int secondFrom;
+int secondTo;
+int sum = 0;
+int sum2 = 0;
+
+foreach (string line in day03Input)
+{
+    firstFrom = Int32.Parse(line.Substring(0, line.IndexOf("-")));
+    firstTo = Int32.Parse(line.Substring(line.IndexOf("-") + 1,line.IndexOf(",") - line.IndexOf("-") - 1));
+    secondFrom = Int32.Parse(line.Substring(line.IndexOf(",") + 1,line.LastIndexOf("-") - line.IndexOf(",") - 1));
+    secondTo = Int32.Parse(line.Split("-").Last());
+    if(secondFrom >= firstFrom && secondTo <= firstTo)
+    {
+        sum++;
+    }
+    else if(firstFrom >= secondFrom && firstTo <= secondTo)
+    {
+        sum++;  
+    }
+    if (firstFrom <= secondTo && secondFrom <= firstTo)
+    {
+        sum2++;
+    }
+}
+Console.WriteLine($"{sum} assignment pairs fully contain the other");
+Console.WriteLine($"{sum2} assignment pairs overlap");
