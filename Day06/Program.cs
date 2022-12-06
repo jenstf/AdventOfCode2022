77a1e599d675d841428827e71666754103c1eb16
Jens Tore Fremmegård <jenstf@gmail.com>
2022-12-06 08:18:28 +0100

    Day06

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -2,41 +2,32 @@
 string text = day06Input[0];
 
 char[] c = text.ToArray();
-int answer = 0;
+int answer1 = 0;
 for (int i = 0; i < c.Count() - 3; i++)
 {
     char[] tempc = new char[4] { c[i], c[i + 1], c[i + 2], c[i + 3] };
     if (tempc.Distinct().Count() == 4)
     {
-        answer = i + 3;
+        answer1 = i + 3;
         break;
     }
 }
 
 int answer2 = 0;
-for (int i = answer; i < c.Count() - 13; i++)
+for (int i = answer1; i < c.Count() - 13; i++)
 {
-    char[] tempc = new char[14];
+    char[] temp = new char[14];
     for (int j = 0; j < 14; j++)
     {
-        tempc[j] = c[i + j];
+        temp[j] = c[i + j];
     }
-    if (tempc.Distinct().Count() == 14)
+    if (temp.Distinct().Count() == 14)
     {
         answer2 = i + 13;
         break;
     }
 }
 
-Console.WriteLine((c[answer - 3]) + "," + (c[answer - 2]) + "," + (c[answer - 1]) + "," + (c[answer]));
-Console.WriteLine(answer + 1); //Part 1 Answer
-
-for (int i = 14; i > 0; i--)
-{
-    Console.Write(c[answer2 + 1 - i]);
-    if (i != 1) { Console.Write(", "); }
-
-    Console.WriteLine();
-    Console.WriteLine(answer2 + 1);
-}
+Console.WriteLine("First start-of-packet marker detected at: " + (answer1 + 1));
+Console.WriteLine("First start-of-message marker detected at: " + (answer2 + 1));
 
