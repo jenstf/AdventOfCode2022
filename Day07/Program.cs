f5648d63b99f762be62b083f58d2d2c759b248e0
Jens Tore Fremmegård <jenstf@gmail.com>
2022-12-07 07:43:09 +0100

    Day07

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines(@"c:\Temp\aoc22day07.txt");
+
+Dir currentDir = new Dir()
+{
+    Path = "/"
+};
+
+List<Dir> directories = new List<Dir>() { currentDir };
+
+
+foreach (string line in lines)
+{
+    if (line.StartsWith("$"))
+    {
+        if (line.StartsWith("$ cd"))
+        {
+            string newFolder = line.Split(' ').Last();
+            if (newFolder == "/")
+            {
+                currentDir = directories.First(o => o.Path == "/");
+            }
+            else if (newFolder == "..")
+            {
+                var split = currentDir.Path.Split('/');
+
+                string newPath = "/" + string.Join('/', split.Take(split.Count() - 1));
+                newPath = newPath.Replace("//", "/");
+                currentDir = directories.First(o => o.Path == newPath);
+            }
+            else
+            {
+                string newPath = Path.Combine(currentDir.Path, newFolder).Replace("\\", "/");
+                currentDir = new Dir() { Path = newPath };
+                directories.Add(currentDir);
+            }
+        }
+    }
+    else if (line.StartsWith("dir"))
+    {
+        continue;
+    }
+    else
+    {
+        int size = int.Parse(line.Split(' ').First());
+
+        foreach (var d in directories.Where(o => currentDir.Path.StartsWith(o.Path)))
+        {
+            d.Size += size;
+        }
+    }
+}
+
+int total = 0;
+
+foreach (var d in directories.Where(o => o.Size < 100000))
+{
+    total += d.Size;
+}
+
+Console.WriteLine("Part1:" + total);
+
+int available = 70000000 - directories.First(o => o.Path == "/").Size;
+
+int smallest = directories.Where(o => o.Size >= (30000000 - available)).Min(o => o.Size);
+
+Console.WriteLine("Part2:" + smallest);
+
+class Dir
+{
+    public string Path { get; set; }
+    public int Size { get; set; }
+}
